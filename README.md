9938c95a570d3f1afabe527ecccb8c60b8fa7545
SSSHIvah <118367239+SSSHIvah@users.noreply.github.com>
2023-10-22 10:46:45 +0300

    Betta version website without backend

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 Save your intellectual property
 
 Colors: https://colorscheme.ru/#3s11Tw0w0v5Lt
-
-#086FA1	#225D7A	#03496A	#5FBBE8	#A2D1E8
---
+++ js/nav.js
@@ -0,0 +1,90 @@
+let header = $("#header");
+
+let intro = $("#intro");
+let whom = $("#whom");
+let timeline = $("#timeline");
+
+let scrollPosLast = $(window).scrollTop();
+let scrollPos = $(window).scrollTop();
+
+let nav1 = $("#nav1");
+let nav2 = $("#nav2");
+let nav3 = $("#nav3");
+
+checkScroll(scrollPosLast, scrollPos);
+chekScrollPos(scrollPos);
+
+$("#headerHover").hover(function () {
+    header.removeClass("fixed");
+});
+
+nav1.mouseenter(function () {
+    let active = $(".nav.active");
+    active.removeClass("active");
+});
+nav1.mouseout(function () {
+    chekScrollPos(scrollPos);
+});
+
+nav2.mouseenter(function () {
+    let active = $(".nav.active");
+    active.removeClass("active");
+});
+nav2.mouseout(function () {
+    chekScrollPos(scrollPos);
+});
+
+nav3.mouseenter(function () {
+    let active = $(".nav.active");
+    active.removeClass("active");
+});
+nav3.mouseout(function () {
+    chekScrollPos(scrollPos);
+});
+
+
+$(window).on("scroll resize", function () {
+    scrollPosLast = scrollPos;
+    scrollPos = $(this).scrollTop();
+
+    chekScrollPos(scrollPos);
+    checkScroll(scrollPosLast, scrollPos);
+});
+
+function checkScroll(scrollPosLast, scrollPos) {
+    if (scrollPos > scrollPosLast) {
+        header.addClass("fixed");
+    } else if (scrollPos < scrollPosLast || scrollPos == 0) {
+        header.removeClass("fixed");
+    }
+}
+
+function chekScrollPos(scrollPos) {
+    let active = $(".nav.active");
+
+    if (scrollPos < whom.offset().top - 200) {
+        active.removeClass("active");
+        nav1.addClass("active");
+    } else if (scrollPos > whom.offset().top - 200 && scrollPos < timeline.offset().top - 200) {
+        active.removeClass("active");
+        nav2.addClass("active");
+    } else if (scrollPos > timeline.offset().top - 200) {
+        active.removeClass("active");
+        nav3.addClass("active");
+    }
+}
+
+
+/* Smooth scroll */
+let elementId;
+let elementOffset;
+
+$("[data-scroll]").on("click", function (eventImportant) {
+    console.log($(this)[0]);
+    elementId = $(this).data("scroll");
+    elementOffset = $(elementId).offset().top;
+
+    $("html, body").animate({
+        scrollTop: elementOffset - 40   
+    }, 700);
+});
